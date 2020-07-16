51483a6c291a71bb3dc748f5008baeb392bd224a
sama0066 <sama0066@algonquinlive.com>
2020-07-15 23:01:50 -0400

    Added Activities

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnGeoData = findViewById(R.id.btnGeoData);
+        btnGeoData.setOnClickListener( v -> {
+            Intent intentGeoData = new Intent(MainActivity.this, GeoDataSource.class);
+            startActivity(intentGeoData);
+        });
+
+        Button btnSoccer = findViewById(R.id.btnSoccer);
+        btnSoccer.setOnClickListener( v -> {
+            Intent intentSoccerMatch = new Intent(MainActivity.this, SoccerMatchHighlights.class);
+            startActivity(intentSoccerMatch);
+        });
+
+        Button btnSongLyrics = findViewById(R.id.btnSongLyrics);
+        btnSongLyrics.setOnClickListener( v -> {
+            Intent intentSongLyrics = new Intent(MainActivity.this, SongLyricsSearch.class);
+            startActivity(intentSongLyrics);
+        });
+
+        Button btnDeezerSong = findViewById(R.id.btnDeezerSong);
+        btnDeezerSong.setOnClickListener( v -> {
+            Intent intentDeezerSong = new Intent(MainActivity.this, DeezerSongSearch.class);
+            startActivity(intentDeezerSong);
+        });
     }
 }
---
+++ app/src/main/java/com/example/finalproject/SoccerMatchHighlights.java
@@ -0,0 +1,14 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoccerMatchHighlights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soccer_match_highlights);
+    }
+}
