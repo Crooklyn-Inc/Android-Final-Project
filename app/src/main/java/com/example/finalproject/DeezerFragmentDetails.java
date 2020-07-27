389f50c3f8879e55cad122f108709350ad60f0d3
Senya <yuliktsv@gmail.com>
2020-07-27 16:11:01 -0400

    Deezer ToolBar added

--- app/src/main/java/com/example/finalproject/DeezerFragmentDetails.java
+++ app/src/main/java/com/example/finalproject/DeezerFragmentDetails.java
@@ -1,19 +1,24 @@
 package com.example.finalproject;
 
+import android.content.Context;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.finalproject.ui.main.DeezerFragmentDetailsFragment;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DeezerFragmentDetails#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class DeezerFragmentDetails extends Fragment {
+
     private Bundle dataFromActivity;
     private long id;
     private AppCompatActivity parentActivity;
