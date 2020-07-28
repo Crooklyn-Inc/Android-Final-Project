fba703ca02ebd9d1724e1b00531f48a6521bd6d2
Olga Zimina <zimi0001@algonquinlive.com>
2020-07-28 02:09:12 -0400

    added search result view

--- app/src/main/java/com/example/finalproject/SongLyricsSearch.java
+++ app/src/main/java/com/example/finalproject/SongLyricsSearch.java
@@ -2,7 +2,9 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SongLyricsSearch extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SongLyricsSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_lyrics_search);
+
+
+        Button btnSongLyrics = findViewById(R.id.slsSearchLyricsBtn);
+        btnSongLyrics.setOnClickListener(v -> {
+            Intent intentSongLyrics = new Intent(SongLyricsSearch.this, SongLyricsSearchResult.class);
+            startActivity(intentSongLyrics);
+        });
+
+
     }
 }
