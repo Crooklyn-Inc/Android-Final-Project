b595ca7bede00a90440a59c4ee24f8f74b0247f5
Olga Zimina <zimi0001@algonquinlive.com>
2020-07-29 00:05:58 -0400

    WiP

--- app/src/main/java/com/example/finalproject/sls/SongLyricsSearchResult.java
+++ app/src/main/java/com/example/finalproject/sls/SongLyricsSearchResult.java
@@ -27,24 +27,22 @@ public class SongLyricsSearchResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_lyrics_search_result);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SongLyricsSearch.PREFERENCES, Context.MODE_PRIVATE);
+        Intent searchData = getIntent();
 
         TextView band = findViewById(R.id.slsGroupName);
         TextView song = findViewById(R.id.slsSongName);
-        band.setText(sharedPreferences.getString(SongLyricsSearch.BAND, ""));
-        song.setText(sharedPreferences.getString(SongLyricsSearch.SONG, ""));
+        band.setText(searchData.getStringExtra(SongLyricsSearch.BAND));
+        song.setText(searchData.getStringExtra(SongLyricsSearch.SONG));
 
         TextView lyrics = findViewById(R.id.slsLyricsName);
 
-        SongLyricsSearchNetwork searchNetwork = new SongLyricsSearchNetwork();
-        searchNetwork.execute(band, song);
-
-        HttpURLConnection openConnection = getOpenConnection(param);
-
-
-        String   text   = "Baby, sometimes I feel like dying, \ndriving while I'm closing my eyes. \nMoving in and out of hiding, \ntrying to catch some truth in my life. \nWatching your stars and your moonlight, \ncome tumbling down from the sky. \n\nTake it now. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do? \n\n\n\nSunday morning, my town is sleeping. \n\nLying all alone in my bed, \n\nthere's not a sound, I can't help but listening. \n\nWishing I was somewhere else instead. \n\nBut sometimes they're too hard to handle, \n\nthese voices inside my head. \n\nListen now. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do? \n\n\n\nTake a walk inside my dream: \n\nA church, a lonely road. \n\nAll the people come and go and come and go. \n\n\n\nI'm gonna run to you, I'm gonna come to you. \n\nDo it now! \n\nI'm gonna run to you, I'm gonna count on you. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do?";
         lyrics.setMovementMethod(new ScrollingMovementMethod());
-        lyrics.setText(text);
+
+        SongLyricsSearchNetwork searchNetwork = new SongLyricsSearchNetwork(findViewById(R.id.slsLyricsName).getRootView());
+        searchNetwork.execute(band.getText().toString(), song.getText().toString());
+
+        //String   text   = "Baby, sometimes I feel like dying, \ndriving while I'm closing my eyes. \nMoving in and out of hiding, \ntrying to catch some truth in my life. \nWatching your stars and your moonlight, \ncome tumbling down from the sky. \n\nTake it now. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do? \n\n\n\nSunday morning, my town is sleeping. \n\nLying all alone in my bed, \n\nthere's not a sound, I can't help but listening. \n\nWishing I was somewhere else instead. \n\nBut sometimes they're too hard to handle, \n\nthese voices inside my head. \n\nListen now. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do? \n\n\n\nTake a walk inside my dream: \n\nA church, a lonely road. \n\nAll the people come and go and come and go. \n\n\n\nI'm gonna run to you, I'm gonna come to you. \n\nDo it now! \n\nI'm gonna run to you, I'm gonna count on you. \n\n\n\nI'm gonna run to you, I'm gonna come to you, \n\nI wanna find you in everything that I do. \n\nI'm gonna run to you, I'm gonna count on you, \n\nI'm gonna follow. Baby, what else can I do?";
+        //lyrics.setText(text);
 
         Button addToFavBtn = findViewById(R.id.slsAddToFavoriteListBtn);
         addToFavBtn.setOnClickListener(btn -> {
@@ -66,19 +64,4 @@ public class SongLyricsSearchResult extends AppCompatActivity {
                 s.getText().toString(),
                 l.getText().toString()));
     }
-
-    private HttpURLConnection getOpenConnection(String link) throws IOException {
-        URL               url        = new URL(link);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setReadTimeout(10000);
-        connection.setConnectTimeout(15000);
-        connection.setRequestMethod("GET");
-        connection.setDoInput(true);
-        connection.connect();
-
-        return connection;
-    }
-
-
 }
